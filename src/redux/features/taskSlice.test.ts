b231510c4858fe1378b41b901888d56b9e99a2ac
Victor Redondo <vrviktor@gmail.com>
2022-11-01 20:50:23 +0100

    Modify taskslice test

--- src/redux/features/taskSlice.test.ts
+++ src/redux/features/taskSlice.test.ts
@@ -6,26 +6,28 @@ import {
 } from "./taskSlice";
 
 describe("Given the tasksReducer function", () => {
-  describe("When it receives a 'loadTasks' action with a list of 2 tasks and an in the payload", () => {
-    test("Then should return a list of 2 tasks", () => {
-      const loadToDoTaskListAction = loadTasksActionCreator(tasks);
-      const initialStat = tasksInitialState;
-      const expectState = {
-        tasks: [
-          {
-            id: 0,
-            name: "Barrer",
-            done: true,
-          },
-          {
-            id: 1,
-            name: "Fregar",
-            done: false,
-          },
-        ],
-      };
-      const newState = tasksReducer(initialStat, loadToDoTaskListAction);
-      expect(newState).toStrictEqual(expectState);
+  describe("When its invoked", () => {
+    describe("And its receives the 'loadTasks' action with a list of 2 tasks", () => {
+      test("Then should return a list of 2 tasks", () => {
+        const loadToDoTaskListAction = loadTasksActionCreator(tasks);
+        const initialStat = tasksInitialState;
+        const expectState = {
+          tasks: [
+            {
+              id: 0,
+              name: "Barrer",
+              done: true,
+            },
+            {
+              id: 1,
+              name: "Fregar",
+              done: false,
+            },
+          ],
+        };
+        const newState = tasksReducer(initialStat, loadToDoTaskListAction);
+        expect(newState).toStrictEqual(expectState);
+      });
     });
   });
 });
